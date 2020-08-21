c3b24df443b3e756f2aab07d510c9717e1479115
Vladimir Karpovich <dev.karpovich@gmail.com>
2020-08-21 17:50:03 +0300

    changes

--- CleanBlackLines.cpp
+++ CleanBlackLines.cpp
@@ -5,8 +5,10 @@ using namespace cv;
 using namespace std;
 
 bool isBlackPixel(Vec3b bgrPixel);
+
 int getPercentOfBlackPixels(double black_pixels, double row);
 vector<bool> getBlackLines(Mat image);
+vector<Vec3b> getNewImage(Mat image, vector<bool> black_lines);
 
 const int BLACK_LIMIT = 27;
 const int PERCENT_OF_BLACK = 75;
@@ -26,8 +28,12 @@ int main(int argc, char **argv) {
         printf("No image data \n");
         return -1;
     }
-    bool *black_lines = getBlackLines(image);
-    cout << black_lines;
+    vector<bool> black_lines = getBlackLines(image);
+    vector<Vec3b> new_image = getNewImage(image, black_lines);
+
+//    for(int i=0; i<black_lines.size(); ++i)
+//        cout << black_lines[i] << "\n";
+
 //    namedWindow("Display Image", WINDOW_AUTOSIZE );
 //    imshow("Display Image", image);
 
@@ -48,25 +54,31 @@ int getPercentOfBlackPixels(double black_pixels, double row) {
 }
 
 
-
 vector<bool> getBlackLines(Mat image) {
-
     int black_pixels[image.cols];
-    vector<bool> black_lines[image.cols];
+    vector<bool> black_lines(image.cols, false);
     for (int i = 0; i < image.rows; i++) {
         black_pixels[i] = 0;
-        black_lines[i] = false;
         for (int j = 0; j < image.cols; j++) {
             Vec3b bgrPixel = image.at<Vec3b>(i, j);
             if (isBlackPixel(bgrPixel)) {
-                black_pixels[i] = true;
+                black_pixels[i]++;
             }
         }
         int p = getPercentOfBlackPixels(black_pixels[i], image.cols);
-
         if (p > PERCENT_OF_BLACK) {
-            *black_lines[i] = true;
+            black_lines[i] = true;
         }
     }
-    return *black_lines;
+    return black_lines;
+}
+vector<Vec3b> getNewImage(Mat image, vector<bool> black_lines)
+{
+    vector<Vec3b> new_image;
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            new_image[i][j] = image.at<Vec3b>(i, j);
+        }
+    }
+    return new_image;
 }
