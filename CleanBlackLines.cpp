4112abf4fa4328aa7645a91a3bd8ced79e560b85
Vladimir Karpovich <dev.karpovich@gmail.com>
2020-08-21 13:36:01 +0300

    backup

--- CleanBlackLines.cpp
+++ CleanBlackLines.cpp
@@ -2,27 +2,45 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
+
+void isBlackPixel(Vec3s bgrPixel);
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+//    if ( argc != 2 )
+//    {
+//        printf("usage: DisplayImage.out <Image_Path>\n");
+//        return -1;
+//    }
 
     Mat image;
-    image = imread( argv[1], 1 );
+//    image = imread( argv[1], 1 );
+    image = imread( "1.jpg", 1 );
 
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    for(int i = 0; i < image.rows; i++)
+    {
+        for(int j = 0; j < image.cols; j++)
+        {
+            Vec3b bgrPixel = image.at<Vec3s>(i, j);
+            isBlackPixel(bgrPixel);
+//            cout << bgrPixel << "\n";
+            // do something with BGR values...
+        }
+    }
+//    namedWindow("Display Image", WINDOW_AUTOSIZE );
+//    imshow("Display Image", image);
 
     waitKey(0);
 
     return 0;
+}
+void isBlackPixel(Vec3s bgrPixel)
+{
+    cout << bgrPixel[0] << " - " << bgrPixel[1]  << " - " << bgrPixel[2]  << "\n";
 }
