6316b96d27dc2086f6e238344e67cc659113c753
Vladimir Karpovich <dev.karpovich@gmail.com>
2020-08-22 13:31:48 +0300

    fix

--- CleanBlackLines.cpp
+++ CleanBlackLines.cpp
@@ -9,21 +9,23 @@ using namespace std;
 namespace fs = std::experimental::filesystem;
 
 bool isBlackPixel(Vec3b bgrPixel);
+
 int getPercentOfBlackPixels(double black_pixels, double row);
+
 string getNewFilePath(char **argv);
+
 string getOldFilePath(char **argv);
 
 const int BLACK_LIMIT = 27;
 const int PERCENT_OF_BLACK = 75;
 
 int main(int argc, char **argv) {
-    if ( argc != 2 )
-    {
+    if (argc != 2) {
         printf("usage: CleanBlackLines <Image_Path>\n");
         return -1;
     }
     Mat image;
-    image = imread( argv[1], 1 );
+    image = imread(argv[1], 1);
     if (!image.data) {
         printf("No image data \n");
         return -1;
@@ -52,40 +54,40 @@ int main(int argc, char **argv) {
             }
         }
     }
-	string old_file_path = getOldFilePath(argv);
-	string new_file_path = getNewFilePath(argv);
+    string old_file_path = getOldFilePath(argv);
+    string new_file_path = getNewFilePath(argv);
 
-    if(changed) {
-      
-		bool is_copyed = false;
+    if (changed) {
 
-		try {
+        bool is_copyed = false;
+
+        try {
             fs::copy_file(old_file_path, new_file_path);
             is_copyed = true;
         }
-        catch (fs::filesystem_error& e) {
+        catch (fs::filesystem_error &e) {
             fprintf(stderr, "Could not copy file : %s\n", e.what());
             return 1;
         }
 
-        if(is_copyed) {
-	        try {
-	            imwrite(old_file_path, image);
-	        }
-	        catch (runtime_error &ex) {
-	            fprintf(stderr, "Exception save image to JPG format: %s\n", ex.what());
-	            printf("Try back copy file\n");
-	            fs::copy_file(new_file_path, old_file_path);
-	            return 1;
-	        }
-	       cout << old_file_path << " - DONE. COPY " << new_file_path << "\n";
+        if (is_copyed) {
+            try {
+                imwrite(old_file_path, image);
+            }
+            catch (runtime_error &ex) {
+                fprintf(stderr, "Exception save image to JPG format: %s\n", ex.what());
+                printf("Try back copy file\n");
+                fs::copy_file(new_file_path, old_file_path);
+                return 1;
+            }
+            cout << old_file_path << " - DONE. COPY " << new_file_path << "\n";
         } else {
-        	printf("Error of copy file. File has not been cahnged\n");
+            printf("Error of copy file. File has not been cahnged\n");
         }
-        
+
         return 0;
     } else {
-    	cout << old_file_path << " - SKIP" << "\n";
+        cout << old_file_path << " - SKIP" << "\n";
     }
 
     waitKey(0);
@@ -103,14 +105,14 @@ bool isBlackPixel(Vec3b bgrPixel) {
 int getPercentOfBlackPixels(double black_pixels, double row) {
     return black_pixels > 0 ? black_pixels / row * 100 : 0;
 }
-string getNewFilePath(char **argv)
-{
-	fs::path absolute_path = fs::absolute(argv[1]);
-    string s (absolute_path);
-  	regex e ("\\.(.*)$");
-  	return std::regex_replace (s,e,"_ORIGIN_.$1");
+
+string getNewFilePath(char **argv) {
+    fs::path absolute_path = fs::absolute(argv[1]);
+    string s(absolute_path);
+    regex e("\\.(.*)$");
+    return std::regex_replace(s, e, "_ORIGIN_.$1");
 }
-string getOldFilePath(char **argv)
-{
-	return fs::absolute(argv[1]);
+
+string getOldFilePath(char **argv) {
+    return fs::absolute(argv[1]);
 }
