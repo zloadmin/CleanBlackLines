fabf9e0fe92adaddf87c8aeb61d02abaee18343b
Vladimir Karpovich <dev.karpovich@gmail.com>
2020-08-22 11:57:11 +0300

    clean black lines

--- CleanBlackLines.cpp
+++ CleanBlackLines.cpp
@@ -5,37 +5,57 @@ using namespace cv;
 using namespace std;
 
 bool isBlackPixel(Vec3b bgrPixel);
-
 int getPercentOfBlackPixels(double black_pixels, double row);
-vector<bool> getBlackLines(Mat image);
-vector<Vec3b> getNewImage(Mat image, vector<bool> black_lines);
 
 const int BLACK_LIMIT = 27;
 const int PERCENT_OF_BLACK = 75;
 
 int main(int argc, char **argv) {
-//    if ( argc != 2 )
-//    {
-//        printf("usage: DisplayImage.out <Image_Path>\n");
-//        return -1;
-//    }
-
+    if ( argc != 2 )
+    {
+        printf("usage: CleanBlackLines <Image_Path>\n");
+        return -1;
+    }
     Mat image;
-//    image = imread( argv[1], 1 );
-    image = imread("1.jpg", 1);
-
+    image = imread( argv[1], 1 );
     if (!image.data) {
         printf("No image data \n");
         return -1;
     }
-    vector<bool> black_lines = getBlackLines(image);
-    vector<Vec3b> new_image = getNewImage(image, black_lines);
-
-//    for(int i=0; i<black_lines.size(); ++i)
-//        cout << black_lines[i] << "\n";
-
-//    namedWindow("Display Image", WINDOW_AUTOSIZE );
-//    imshow("Display Image", image);
+    int black_pixels[image.cols];
+    bool changed = false;
+    for (int i = 0; i < image.rows; i++) {
+        black_pixels[i] = 0;
+        for (int j = 0; j < image.cols; j++) {
+            Vec3b bgrPixel = image.at<Vec3b>(i, j);
+            if (isBlackPixel(bgrPixel)) {
+                black_pixels[i]++;
+            }
+        }
+        int p = getPercentOfBlackPixels(black_pixels[i], image.cols);
+        if (p > PERCENT_OF_BLACK) {
+            changed = true;
+            for (int j = 0; j < image.cols; j++) {
+                Vec3b bgrPixel = image.at<Vec3b>(i, j);
+                bgrPixel[0] = 0;
+                bgrPixel[1] = 0;
+                bgrPixel[2] = 0;
+                image.at<Vec3b>(i, j) = bgrPixel;
+            }
+        }
+    }
+    if(changed) {
+        printf("Detected black lines, file will change\n");
+        try {
+            imwrite("test.jpg", image);
+        }
+        catch (runtime_error &ex) {
+            fprintf(stderr, "Exception save image to JPG format: %s\n", ex.what());
+            return 1;
+        }
+        fprintf(stdout, "Saved JPG file.\n");
+        return 0;
+    }
 
     waitKey(0);
 
@@ -51,34 +71,4 @@ bool isBlackPixel(Vec3b bgrPixel) {
 
 int getPercentOfBlackPixels(double black_pixels, double row) {
     return black_pixels > 0 ? black_pixels / row * 100 : 0;
-}
-
-
-vector<bool> getBlackLines(Mat image) {
-    int black_pixels[image.cols];
-    vector<bool> black_lines(image.cols, false);
-    for (int i = 0; i < image.rows; i++) {
-        black_pixels[i] = 0;
-        for (int j = 0; j < image.cols; j++) {
-            Vec3b bgrPixel = image.at<Vec3b>(i, j);
-            if (isBlackPixel(bgrPixel)) {
-                black_pixels[i]++;
-            }
-        }
-        int p = getPercentOfBlackPixels(black_pixels[i], image.cols);
-        if (p > PERCENT_OF_BLACK) {
-            black_lines[i] = true;
-        }
-    }
-    return black_lines;
-}
-vector<Vec3b> getNewImage(Mat image, vector<bool> black_lines)
-{
-    vector<Vec3b> new_image;
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            new_image[i][j] = image.at<Vec3b>(i, j);
-        }
-    }
-    return new_image;
 }
